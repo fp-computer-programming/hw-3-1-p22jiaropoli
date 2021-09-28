f8cc8cbdefa215b1fe1573a2192fd894a1a4108f
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2021-09-28 10:20:14 -0400

    Add file

---
+++ hw3-1-1.py
@@ -0,0 +1,13 @@
+# Author JRI 9/28/21
+
+# input values for team 1
+wins_team1 = input("How many wins did team 1 have?")
+ties_team1 = input("How many ties did team 1 have?")
+
+# input values for team 2
+wins_team2 = input("How many wins did team 2 have?")
+ties_team2 = input("How many ties did team 2 have?")
+
+# total point
+team1_points = (int(wins_team1) * 3) + int(ties_team1)
+team2_points = (int(wins_team2) * 3) + int(ties_team2)

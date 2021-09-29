27fecbc44fb8453f48a7bba5c2f4df9f6da9acbb
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2021-09-29 10:20:29 -0400

    Add file

--- hw3-1-1.py
+++ hw3-1-1.py
@@ -1,13 +1,19 @@
 # Author JRI 9/28/21
 
 # input values for team 1
-wins_team1 = input("How many wins did team 1 have?")
-ties_team1 = input("How many ties did team 1 have?")
+wins_team1 = input("How many wins did team 1 have? ")
+ties_team1 = input("How many ties did team 1 have? ")
 
 # input values for team 2
-wins_team2 = input("How many wins did team 2 have?")
-ties_team2 = input("How many ties did team 2 have?")
+wins_team2 = input("How many wins did team 2 have? ")
+ties_team2 = input("How many ties did team 2 have? ")
 
 # total point
 team1_points = (int(wins_team1) * 3) + int(ties_team1)
 team2_points = (int(wins_team2) * 3) + int(ties_team2)
+
+# if statements
+if team1_points >= team2_points:
+    print("team 1 had more points")
+else:
+    print("team 2 had more points")
---
+++ hw3-1-2.py
@@ -0,0 +1,4 @@
+# Author JRI 9/29/21
+
+card1_value = input("enter value of your first card")
+card2_value = input("enter value of your second card")

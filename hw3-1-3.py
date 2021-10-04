3a44691d5be265dba7ab50da0046997851905ad4
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2021-10-04 10:09:08 -0400

    Add file

--- hw3-1-3.py
+++ hw3-1-3.py
@@ -1,4 +1,10 @@
 # Author JRI 9/30/21
 
-x = int(input("input a number"))
+x = int(input("input a number "))
 
+if x % 2 == 0:
+    print("x is divisible by 2")
+elif x % 3 == 0:
+    print("x is divisible by 3")
+elif x % 5 == 0:
+    print("x is divisible by 5")
